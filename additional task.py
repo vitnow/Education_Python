c3b0ac1862d6c24f22effc964288c27d3794e99c
ViToN <vitnow2009@gmail.com>
2024-11-08 14:13:52 +0300

    del comments additional task

--- additional task.py
+++ additional task.py
@@ -1,11 +1,3 @@
-# L = [4, 5, 1, 2, 9, 7, 10, 8]
-# # using sum() method
-# count = sum(L)
-# # finding average
-# avg = count / len(L)
-#
-# print("sum = ", count)
-# print("average = ", avg)
 
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
