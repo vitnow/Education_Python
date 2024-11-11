74c18004a07ba5ea43c4ed09dbe7551792cdc0cd
ViToN <vitnow2009@gmail.com>
2024-11-11 20:41:09 +0300

    Add examples of table and enumeration

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+# # проверяет и выводит числа по условию без остатка до введеного числа
+# number = int(input('Введите число: '))
+#
+# for i in range(1, number + 1):
+#     if number % i == 0:
+#         print(i)
+# Таблица умножения
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#        print(f'{i} x {j} = {i * j}')
 
 
-
-x = 43
-
-y = 32
-
-print(x * y)
-
-print("End line")
