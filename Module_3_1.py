48ba098d7a8b0072c9b4083cd5c3ac8e61085af5
ViToN <vitnow2009@gmail.com>
2024-11-22 10:04:23 +0300

    Made task Module_3_1

--- Module_3_1.py
+++ Module_3_1.py
@@ -6,10 +6,27 @@
 # False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
 # Пункты задачи:
 # Создать переменную calls = 0 вне функций.
-# Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
+# Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться
+# в остальных двух функциях.
 # Создать функцию string_info с параметром string и реализовать логику работы по описанию.
 # Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
 # Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
 # Вывести значение переменной calls на экран(в консоль).
 
 calls = 0
+def count_calls ():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBan
+print(is_contains('cycle', ['recycle', 'cyclic'])) # No matches
+print(calls)
