d9cf1e64e63e7ec921bd69aed83c4d464a53912c
ViToN <vitnow2009@gmail.com>
2024-11-24 09:46:33 +0300

    Make task Module_3_4

---
+++ Module_3_4.py
@@ -0,0 +1,22 @@
+# Пункты задачи:
+# # Объявите функцию single_root_words и напишите в ней параметры root_word и *other_words.
+# Создайте внутри функции пустой список same_words, который пополнится нужными словами.
+# При помощи цикла for переберите предполагаемо подходящие слова.
+# Пропишите корректное относительно задачи условие, при котором добавляются слова в результирующий список same_words.
+# После цикла верните образованный функцией список same_words.
+# Вызовите функцию single_root_words и выведете на экран(консоль) возвращённое ей значение.
+# Пример результата выполнения программы:
+# # Исходный код:
+# # result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+# # result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+# # print(result1)
+# # print(result2)
+# # Вывод на консоль:
+# # ['richiest', 'orichalcum', 'richies']
+# # ['Able', 'Disable']
+
+def single_root_words(root_word, *other_words):
+    same_words =[]
+    root_word = root_word.upper()
+    for s in other_words.upper()
+        if same_words
