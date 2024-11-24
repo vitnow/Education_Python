45bb9b940d0fbdfca628f1ff693bd08d0ff7dba9
ViToN <vitnow2009@gmail.com>
2024-11-24 10:07:15 +0300

    Made task Module_3_4

--- Module_3_4.py
+++ Module_3_4.py
@@ -17,6 +17,13 @@
 
 def single_root_words(root_word, *other_words):
     same_words =[]
-    root_word = root_word.upper()
-    for s in other_words.upper()
-        if same_words
+    for i in other_words:
+        if root_word.upper() in i.upper() or i.upper() in root_word.upper():
+            same_words.append(i)
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
