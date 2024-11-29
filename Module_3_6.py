478ec7eb7a241e6a69026975d90d262bde8e46fc
ViToN <vitnow2009@gmail.com>
2024-11-29 15:14:24 +0300

    Made task Module_3_6

--- Module_3_6.py
+++ Module_3_6.py
@@ -7,9 +7,23 @@
 # Для примера, указанного выше, расчёт вёлся следующим образом:
 #
 # 1 + 2 + 3 + len('a') + 4 + len('b') + 5 + 6 + len('cube') + 7 + .... + 35 = 99
+from mimetypes import inited
 
+def calculate_structure_sum(data_structure):
+    sum = 0
 
-
+    if isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            sum += calculate_structure_sum(item)
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            sum += calculate_structure_sum(key)
+            sum += calculate_structure_sum(value)
+    elif isinstance(data_structure, (int, float)):
+        sum += data_structure
+    elif isinstance(data_structure, str):
+        sum += len(data_structure)
+    return sum
 
 data_structure = [
 [1, 2, 3],
@@ -17,12 +31,6 @@ data_structure = [
 (6, {'cube': 7, 'drum': 8}),
 "Hello",
 ((), [{(2, 'Urban', ('Urban2', 35))}])]
-
-print(type(data_structure))
-
-def calculate_structure_sum(data_structure):
-    pass
-
 
 result = calculate_structure_sum(data_structure)
 
