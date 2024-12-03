347554190dc431d87996699284d727a37c522bca
ViToN <vitnow2009@gmail.com>
2024-12-03 17:41:00 +0300

    Made Module_4_2

--- Module_4_2.py
+++ Module_4_2.py
@@ -15,6 +15,6 @@ def test_function():
         print("Я в области видимости функции test_function")
     inner_function()
 
-test_function()
+test_function()  # Выводит сообщение ---Я в области видимости функции test_function
 
 #inner_function() -- ОШИБКА ---NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
