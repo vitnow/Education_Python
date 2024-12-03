4114fe57307243b876b8ca32efd801f79f8cb2e6
ViToN <vitnow2009@gmail.com>
2024-12-03 17:38:01 +0300

    Add Module_4_2

--- Module_4_2.py
+++ Module_4_2.py
@@ -7,3 +7,14 @@
 #     "Я в области видимости функции test_function"
 # Вызовите функцию inner_function внутри функции test_function
 # Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
+
+
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
+
+#inner_function() -- ОШИБКА ---NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
