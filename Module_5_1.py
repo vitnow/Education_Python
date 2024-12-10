f46839c60431ede049d7f4e1448d2904287f5ca1
ViToN <vitnow2009@gmail.com>
2024-12-10 20:16:45 +0300

    Made new Module_5_1

--- Module_5_1.py
+++ Module_5_1.py
@@ -13,3 +13,23 @@
 # Создайте метод go_to с параметром new_floor и напишите логику внутри него на основе описания задачи.
 # Создайте объект класса House с произвольным названием и количеством этажей.
 # Вызовите метод go_to у этого объекта с произвольным числом.
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if 1<= new_floor <= self.number_of_floors:
+                print(i)
+            else:
+                print('Такого этажа не существует')
+                break
+
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
