da5d9edd0e200ceb53778e924e7d70bb66823d2f
ViToN <vitnow2009@gmail.com>
2025-01-23 18:30:37 +0300

    Add Module_6_hard

---
+++ Module_6_hard.py
@@ -0,0 +1,23 @@
+# class Human:
+#     head = True
+#     _legs = True
+#     __arms = True
+#
+#     def say_hello(self):
+#         print ('Здравствуйте')
+#
+# class Student(Human):
+#
+#     def about(self):
+#         print('Я студент')
+#
+#
+# class Teacher(Human):
+#     def about(self):
+#         print('Я учитель')
+#
+# human = Human()
+# student = Student()
+# teacher = Teacher()
+# student.say_hello(), student.about()
+# teacher.say_hello(), teacher.about()
